1da4f12d57c7013b0ad2411f97283a9877fc9af8
Sampan Verma <sampan.developer@gmail.com>
2020-03-08 02:39:53 +0530

    Added database SQL file for taxonomy update

---
+++ expo.sql
@@ -0,0 +1,338 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2020 at 10:09 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `expo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taxonomy`
+--
+
+CREATE TABLE `taxonomy` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taxonomy_category`
+--
+
+CREATE TABLE `taxonomy_category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `taxonomy` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taxonomy_links`
+--
+
+CREATE TABLE `taxonomy_links` (
+  `category` int(11) NOT NULL,
+  `link` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_authentication`
+--
+
+CREATE TABLE `user_authentication` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `role` int(11) NOT NULL,
+  `date_created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_authentication`
+--
+
+INSERT INTO `user_authentication` (`id`, `name`, `username`, `email`, `password`, `role`, `date_created`) VALUES
+(1, 'Super Admin', 'superadmin', 'himself@samlovescoding.com', '$2y$10$6.IfJ8tRaHOp5JHs9r6O/O0fyd7rB6siyQ5lPBmX/7Sv6RwOKZHfG', 2, '2020-02-22 16:01:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_authentication_tokens`
+--
+
+CREATE TABLE `user_authentication_tokens` (
+  `id` int(11) NOT NULL,
+  `token` varchar(256) NOT NULL,
+  `user` int(11) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_verified` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_details`
+--
+
+CREATE TABLE `user_details` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `meta` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_information`
+--
+
+CREATE TABLE `user_information` (
+  `id` int(11) NOT NULL,
+  `detail` int(11) NOT NULL,
+  `information` text NOT NULL,
+  `user` int(11) NOT NULL,
+  `date_modified` datetime NOT NULL,
+  `date_created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_logs`
+--
+
+CREATE TABLE `user_logs` (
+  `id` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `user` int(11) NOT NULL,
+  `ip_address` varchar(256) NOT NULL,
+  `date_logged` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_logs`
+--
+
+INSERT INTO `user_logs` (`id`, `type`, `message`, `user`, `ip_address`, `date_logged`) VALUES
+(1, 'auth', 'logout', 1, '::1', '2020-03-01 21:07:56'),
+(2, 'auth', 'login', 1, '::1', '2020-03-01 21:08:01'),
+(3, 'auth', 'login', 1, '::1', '2020-03-07 12:10:59'),
+(4, 'auth', 'login', 1, '::1', '2020-03-07 21:54:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_permissions`
+--
+
+CREATE TABLE `user_permissions` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_permissions`
+--
+
+INSERT INTO `user_permissions` (`id`, `name`) VALUES
+(1, 'user permissions'),
+(2, 'user roles'),
+(3, 'user management'),
+(4, 'user details'),
+(5, 'user logs'),
+(6, 'taxonomy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `parent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`id`, `name`, `parent`) VALUES
+(1, 'Subscriber', 0),
+(2, 'Super Admin', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles_permissions`
+--
+
+CREATE TABLE `user_roles_permissions` (
+  `permission` int(11) NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_roles_permissions`
+--
+
+INSERT INTO `user_roles_permissions` (`permission`, `role`) VALUES
+(1, 2),
+(2, 2),
+(3, 2),
+(4, 2),
+(5, 2),
+(6, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `taxonomy`
+--
+ALTER TABLE `taxonomy`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `taxonomy_category`
+--
+ALTER TABLE `taxonomy_category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `user_authentication`
+--
+ALTER TABLE `user_authentication`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `user_authentication_tokens`
+--
+ALTER TABLE `user_authentication_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`);
+
+--
+-- Indexes for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_information`
+--
+ALTER TABLE `user_information`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_logs`
+--
+ALTER TABLE `user_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_permissions`
+--
+ALTER TABLE `user_permissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `taxonomy`
+--
+ALTER TABLE `taxonomy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `taxonomy_category`
+--
+ALTER TABLE `taxonomy_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_authentication`
+--
+ALTER TABLE `user_authentication`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_authentication_tokens`
+--
+ALTER TABLE `user_authentication_tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_details`
+--
+ALTER TABLE `user_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_information`
+--
+ALTER TABLE `user_information`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user_logs`
+--
+ALTER TABLE `user_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_permissions`
+--
+ALTER TABLE `user_permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
